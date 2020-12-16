99f4eb2da3a961d011f91ac769a84fe6c6a4ae01
jpicchi18 <jpicchi18@gmail.com>
2020-12-16 13:06:25 -0800

    cleaned up

--- resulaj.py
+++ resulaj.py
@@ -20,9 +20,9 @@ GLOBAL VARIABLES @
 
 n_trials = 50
 dot_density = 16.7      # measured in dots/(degree^2 * sec)
-n_sets = 3 # each contains n_dots dots. cycle between them in round-robin fashion. for n_sets=2, set 1 in frame 1, set 2 in frame 2, set 1 in frame 3, etc.
+n_sets = 1 # each contains n_dots dots. cycle between them in round-robin fashion. for n_sets=2, set 1 in frame 1, set 2 in frame 2, set 1 in frame 3, etc.
 
-coherence = .1             #Proportion of dots to move together, range from 0 to 1
+coherence = .2             #Proportion of dots to move together, range from 0 to 1
 dot_radius = 2             #Radius of each dot in pixels
 dot_life = 20               # How many frames a dot follows its trajectory before redrawn. -1
                             # is infinite life
